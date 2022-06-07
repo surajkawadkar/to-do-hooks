2182bb86830432c7079bdabc81a67c63e9e54934
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2022-06-07 05:44:54 +0530

    todo using hooks

---
+++ src/Components/Todo.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function ToDo() {
+
+    const [element, setElement]= useState("");
+    const [bucket,setBucket]= useState([]);
+
+    // Helper function
+    function addElement() {
+
+        const item = {
+            id:Math.floor(Math.random()*1000),
+            value:element
+        };
+
+        setBucket(oldList =>[...oldList,item]);
+        setElement("");
+        
+    }
+
+    function deleteElement(id){
+        const newArray = bucket.filter(item=>item.id!==id)
+        setBucket(newArray)
+    }
+    return(
+        <div>
+            <h1>ToDo List</h1>
+            <input type="text"
+            placeholder="enter the element"
+            value={element}
+            onChange={e=>setElement(e.target.value)}>
+            </input>
+            <button type="button" onClick={addElement}>Add</button>
+         
+            <ul>
+
+                {bucket.map(item=>{
+                    return(
+                        <li key= {item.id} > {item.value}   <button onClick={()=>deleteElement(item.id)} >❌</button></li>
+
+                    )
+                })}
+            </ul>
+
+        </div>
+        
+
+
+
+
+    )
+}
+export default ToDo;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,12 @@
+import React , {useState} from 'react'
+
+function TodoForm() {
+    const [input,setInput]=useState("");
+const abc={name:'suraj'};
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+export default TodoForm
+
